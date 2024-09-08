7dfd07bf6ba3fa4808f43a724924a35ced5decab
abhijeetGithu <abhijeetas8660211@gmail.com>
2024-09-08 09:02:36 +0530

    requirements is added

--- myenv/Scripts/activate.bat
+++ myenv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Lenovo\Desktop\end-to-end-mlflow-project\MLflow-project-Machine-learning\myenv
+set VIRTUAL_ENV=D:\Machine-learning-projects\end-to-end-mlflow-project\MLflow-project-Machine-learning\myenv
 
 if not defined PROMPT set PROMPT=$P$G
 
